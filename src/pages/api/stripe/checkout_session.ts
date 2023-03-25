f2832a732fdce1fe53afcc2143fecdb55ddbe35a
mrperrytpx <tomislav.siprak1996@gmail.com>
2023-03-25 13:27:29 +0100

    stripe checkout first pass

---
+++ src/pages/api/stripe/checkout_session.ts
@@ -0,0 +1,67 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import Stripe from "stripe";
+import { TCartProduct } from "../../../redux/slices/cartSlice";
+
+const stripe = new Stripe(process.env.STRIPE_PRIVATE_KEY!, {
+    apiVersion: "2022-11-15",
+    typescript: true,
+});
+
+export function formatAmountForStripe(amount: number, currency: string): number {
+    let numberFormat = new Intl.NumberFormat(["en-US"], {
+        style: "currency",
+        currency: currency,
+        currencyDisplay: "symbol",
+    });
+    const parts = numberFormat.formatToParts(amount);
+    let zeroDecimalCurrency: boolean = true;
+    for (let part of parts) {
+        if (part.type === "decimal") {
+            zeroDecimalCurrency = false;
+        }
+    }
+    return zeroDecimalCurrency ? amount : Math.round(amount * 100);
+}
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method === "POST") {
+        const { cartItems, address }: { cartItems: TCartProduct[]; address: string } = req.body;
+
+        // console.log(cartItems, "ADDY: ", address);
+
+        const params: Stripe.Checkout.SessionCreateParams = {
+            submit_type: "pay",
+            mode: "payment",
+            payment_method_types: ["card"],
+            line_items: [
+                ...cartItems.map((item) => ({
+                    price_data: {
+                        currency: "eur",
+                        unit_amount: formatAmountForStripe(+item.price, "eur"),
+                        product_data: {
+                            name: item.name,
+                            images: [item.variant_image],
+                            description: `Size: ${item.size}, Color: ${item.color_name}`,
+                        },
+                    },
+                    quantity: item.quantity,
+                })),
+            ],
+            success_url: "http://localhost:3000/success",
+            cancel_url: "http://localhost:3000",
+        };
+
+        // console.log("PARAMS", params);
+
+        const checkoutSession: Stripe.Checkout.Session = await stripe.checkout.sessions.create(
+            params
+        );
+
+        res.json(checkoutSession);
+    } else {
+        res.setHeader("Allow", "POST");
+        res.status(405).end("Method Not Allowed");
+    }
+}
+
+export default handler;
--- src/pages/checkout/index.tsx
+++ src/pages/checkout/index.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { cartSelector } from "../../redux/slices/cartSlice";
+import { cartSelector, TCartProduct } from "../../redux/slices/cartSlice";
 import { useRouter } from "next/router";
+import axios from "axios";
+import { CartProduct } from "../../components/CartProduct";
+import Stripe from "stripe";
+import getStripe from "../../utils/getStripe";
+
+// const checkout = async (lineItems: TCartProduct[]) => {};
 
 const CheckoutPage = () => {
   const productsInCart = useSelector(cartSelector);
@@ -12,12 +18,42 @@ const CheckoutPage = () => {
     return <div className="flex-1">Redirecting to shop</div>;
   }
 
+  async function handleCompleteOrder() {
+    const checkoutResponse = await axios.post("http://localhost:3000/api/stripe/checkout_session", {
+      cartItems: productsInCart,
+      address: "test",
+    });
+
+    const checkoutSession: Stripe.Checkout.Session = checkoutResponse.data;
+
+    // console.log("Session:", checkoutSession);
+
+    if ((checkoutSession as any).statusCode === 500) {
+      console.error((checkoutSession as any).message);
+      return;
+    }
+
+    const stripe = await getStripe();
+    const { error } = await stripe!.redirectToCheckout({
+      sessionId: checkoutSession.id,
+    });
+
+    console.warn(error.message);
+  }
+
   return (
     <div className="m-auto flex w-full max-w-screen-md flex-1 flex-col items-center justify-start gap-2">
       <div className="w-full rounded-md border-2 p-6">Cart Overview</div>
+      <div className="flex w-full flex-col items-center gap-4">
+        {productsInCart.map((product) => (
+          <CartProduct key={product.sku} product={product} />
+        ))}
+      </div>
       <div className="w-full rounded-md border-2 p-6">Shipping Cost & VAT</div>
       <div className="w-full rounded-md border-2 p-6">Order Overview</div>
-      <button className="border-md self-end border p-4">Submit & Pay</button>
+      <button type="button" onClick={handleCompleteOrder} className="border-md self-end border p-4">
+        Submit & Pay
+      </button>
     </div>
   );
 };
