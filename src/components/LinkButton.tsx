d15447d8adff3c99570e96d6df17926f1d2f1842
mrperrytpx <tomislav.siprak1996@gmail.com>
2023-04-18 13:06:36 +0200

    Customs disclaimer. some /my_orders styling

--- src/components/LinkButton.tsx
+++ src/components/LinkButton.tsx
@@ -11,7 +11,10 @@ interface ILinkButtonProps {
 export const LinkButton = ({ children, href, className, minWidth, ...rest }: ILinkButtonProps) => {
     return (
         <Link
-            className={className}
+            className={
+                "rounded-lg border py-2 px-4 text-center font-medium uppercase shadow-lg transition-all hover:bg-slate-500 hover:text-white " +
+                className
+            }
             href={href}
             style={{
                 minWidth: minWidth + "px" || 0,
--- src/pages/api/printful/get_order.ts
+++ src/pages/api/printful/get_order.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { TOrder, TOrderResponse } from "../stripe/webhooks";
+import { TOrderResponse } from "../stripe/webhooks";
 import { printfulApiKeyInstance } from "../../../utils/axiosClients";
 import { tryCatchAsync } from "../../../utils/tryCatchWrappers";
 
