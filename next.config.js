598f55a6153668c514e220c0e13fc4413df5fd15
mrperrytpx <tomislav.siprak1996@gmail.com>
2023-05-15 20:16:40 +0200

    next config remove thing

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-    enabled: process.env.ANALYZE === "true",
-    openAnalyzer: false,
-});
 
 const nextConfig = {
     reactStrictMode: true,
@@ -29,4 +25,4 @@ const nextConfig = {
     },
 };
 
-module.exports = withBundleAnalyzer(nextConfig);
+module.exports = nextConfig;
