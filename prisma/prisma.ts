b8f6fc2aa2cf9f7ce47910f82f4d75b2c56c7dac
mrperrytpx <tomislav.siprak1996@gmail.com>
2024-03-13 16:23:30 +0100

    swap away from planestcale to turso sqlite

--- prisma/prisma.ts
+++ prisma/prisma.ts
@@ -1,9 +1,20 @@
 import { PrismaClient } from "@prisma/client";
+import { createClient } from "@libsql/client";
+import { PrismaLibSQL } from "@prisma/adapter-libsql";
+
+// https://www.prisma.io/docs/orm/overview/databases/sqlite
+
+const libsql = createClient({
+    url: `${process.env.TURSO_DATABASE_URL}`,
+    authToken: `${process.env.TURSO_AUTH_TOKEN}`,
+});
+
+const adapter = new PrismaLibSQL(libsql);
 
 const globalForPrisma = global as unknown as {
     prisma: PrismaClient | undefined;
 };
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
